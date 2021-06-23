f00acb9ba229c68d41fe385cb6bad810806af817
Venkatesh Kadali <vkadali@inmoment.com>
2021-06-22 22:09:01 -0600

    latest-renamings

--- base-project/script.js
+++ base-project/script.js
@@ -1,35 +1,32 @@
 
 const endPoint = 'http://localhost:2224/open-port/';
-async function getOpenOpenDetail(hostname){
+async function getOpenDetail(hostname){
     let finalUrl = endPoint+hostname;
-    let response = await fetch(finalUrl);
-    let data = await response.json();
-   createTile(data);
-   // console.log(data);
+    let nMapResponse = await fetch(finalUrl);
+    let data = await nMapResponse.json();
+    createTile(data);
 }
 
 function createTile(data){
 
     let hostName = data.hostName;
     let latest = data.latestScannedPorts;
-    let history = data.historyScannedPort;
-    let newOnes = data.newlyAddedPort;
-    let deleted = data.deleteddPort;
+    let history = data.historyScannedPorts;
+    let newOnes = data.newlyAddedPorts;
+    let deleted = data.deletedPorts;
 
     console.log("Hostname:"+ hostName);
-    console.log("Latest Ports:");
     let parent1 = document.getElementById("open_port");
     while (parent1.firstChild) {
         parent1.removeChild(parent1.firstChild);
     }
     latest.forEach(e => {
-
         const tile = document.createElement("div");
         tile.className = "tiles"
         tile.innerHTML=`port:`+e.portNumber+`<br>protocol:`+e.portProtocol
         parent1.appendChild(tile);
-        });
-    console.log("History Ports:");
+    });
+
     const parent2 = document.getElementById("history_port");
     while (parent2.firstChild) {
         parent2.removeChild(parent2.firstChild);
@@ -41,37 +38,39 @@ function createTile(data){
             tile.innerHTML=`port:`+element.portNumber+`<br>protocol:`+element.portProtocol
             parent2.appendChild(tile);
         });
-      
     });
-    console.log("NewOnes Ports:");
+
     const parent3 = document.getElementById("new_port");
     while (parent3.firstChild) {
         parent3.removeChild(parent3.firstChild);
     }
     newOnes.forEach(e => {
         const tile = document.createElement("div");
-            tile.className = "tiles"
-            tile.innerHTML=`port:`+e.portNumber+`<br>protocol:`+e.portProtocol
-            parent3.appendChild(tile);
+        tile.className = "tiles"
+        tile.innerHTML=`port:`+e.portNumber+`<br>protocol:`+e.portProtocol
+        parent3.appendChild(tile);
     });
-    console.log("Deleted Ports:");
+
     const parent4 = document.getElementById("deleted_port");
     while (parent4.firstChild) {
         parent4.removeChild(parent4.firstChild);
     }
     deleted.forEach(e => {
         const tile = document.createElement("div");
-            tile.className = "tiles"
-            tile.innerHTML=`port:`+e.portNumber+`<br>protocol:`+e.portProtocol
-            parent4.appendChild(tile);
+        tile.className = "tiles"
+        tile.innerHTML=`port:`+e.portNumber+`<br>protocol:`+e.portProtocol
+        parent4.appendChild(tile);
     });
-
-
-
-
 } 
 
-function clickSubmit(){
+function addOrRemoveloaderAndBlockPointerEvent() {
+    let body_block = document.getElementById('body');
+    let loader = document.getElementById('loader_container');
+    body_block.classList.toggle("block-action");
+    loader.classList.toggle("loader");
+}
+
+async function clickSubmit(){
     let hostname = document.getElementById("hostname");
     let text = hostname.value;
     const IPCHECK_REGEX = /^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9\-\.]){1,61}(?:\.[a-zA-Z]{2,})+|\[(?:(?:(?:[a-fA-F0-9]){1,4})(?::(?:[a-fA-F0-9]){1,4}){7}|::1|::)\]|(?:(?:[0-9]{1,3})(?:\.[0-9]{1,3}){3}))(?:\:[0-9]{1,5})?$/;
@@ -81,6 +80,8 @@ function clickSubmit(){
         return false;
     }
     if(text){
-        getOpenOpenDetail(text);
+        addOrRemoveloaderAndBlockPointerEvent();
+        await getOpenDetail(text);
+        addOrRemoveloaderAndBlockPointerEvent();
     }
-}
+}  
